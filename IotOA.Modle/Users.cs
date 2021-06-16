6df43e950c47b634573e080e145f4799fde0af2c
dell <dell@DESKTOP-4FOHNG7>
2021-06-16 16:02:21 +0800

    添加项目文件。

---
+++ IotOA.Modle/Users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IotOA.Modle
+{
+    public class Users
+    {
+        public int UId { get; set; }
+        public string UName { get; set; }
+
+        public int PId { get; set; }
+    }
+}
---
+++ IotOA.Repository/DapperHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace IotOA.Repository
+{
+    public class DapperHelper<T>
+    {
+        public static object Salary(string sql)
+        {
+            using (IDbConnection conn = new SqlConnection("SQLConn"))
+            {
+                conn.Open();
+                var result = conn.ExecuteScalar(sql);
+
+                return result;
+            }
+        }
+    }
+}
